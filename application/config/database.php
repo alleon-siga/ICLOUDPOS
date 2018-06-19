f51dee3092042e901b5d3a8c37431e20f101847c
Mario Delgado <mariodavid.delgado@outlook.com>
2018-06-19 09:46:30 -0500

    nombre de base

--- application/config/database.php
+++ application/config/database.php
@@ -54,7 +54,7 @@ $db['default'] = array(
     'hostname' => 'localhost',
     'username' => 'root',
     'password' => 'nexus1108',
-    'database' => DATABASE_HOST != false ? DATABASE_HOST : 'icloudpos_ochoa2',
+    'database' => DATABASE_HOST != false ? DATABASE_HOST : 'icloudpos_ochoa',
     'dbdriver' => 'mysqli',
     'dbprefix' => '',
     'pconnect' => FALSE,
