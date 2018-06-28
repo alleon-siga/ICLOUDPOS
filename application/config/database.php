5a00abacb19016d4defcddf5cc0a6ae73a7448e9
Antonio Martin <tonitim13@gmail.com>
2018-06-28 12:09:54 -0500

    facturacion electronica, cambios

--- application/config/database.php
+++ application/config/database.php
@@ -51,10 +51,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
     'dsn' => '',
-    'hostname' => 'localhost',
-    'username' => 'root',
-    'password' => 'nexus1108',
-    'database' => DATABASE_HOST != false ? DATABASE_HOST : 'icloudpos_ochoa',
+    'hostname' => '10.1.1.3',
+    'username' => 'user',
+    'password' => 'password',
+    'database' => DATABASE_HOST != false ? DATABASE_HOST : 'crdigital',
     'dbdriver' => 'mysqli',
     'dbprefix' => '',
     'pconnect' => FALSE,
