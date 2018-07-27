6988ff5f5954b76142921e7febdd849cffad499b
MarioDelgado <mariodavid.delgado@outlook.com>
2018-07-26 19:27:51 -0500

    REQ-0001: Script para insertar los accesos al movil

---
+++ script/MENU/2018-07-21 Opciones acceso movil.sql
@@ -0,0 +1,4 @@
+INSERT INTO opcion (nOpcion, nOpcionClase, cOpcionDescripcion, cOpcionNombre) VALUES ('11', null, 'accesomovil', 'Acceso Movil');
+INSERT INTO opcion (nOpcion, nOpcionClase, cOpcionDescripcion, cOpcionNombre) VALUES ('1101', '11', 'ventas', 'Ventas');
+INSERT INTO opcion (nOpcion, nOpcionClase, cOpcionDescripcion, cOpcionNombre) VALUES ('1102', '11', 'registroventas', 'Registro de Ventas');
+INSERT INTO opcion (nOpcion, nOpcionClase, cOpcionDescripcion, cOpcionNombre) VALUES ('1103', '11', 'cliente', 'Cliente');
