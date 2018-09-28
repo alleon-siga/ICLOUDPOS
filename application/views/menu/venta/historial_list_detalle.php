ef39b9f931593587978dbdbbff00193ae01d2dfc
Jorge Ramirez New <jlramirez1210@gmail.com>
2018-09-28 10:06:06 -0500

    correccion en texto.

--- application/views/menu/venta/historial_list_detalle.php
+++ application/views/menu/venta/historial_list_detalle.php
@@ -519,7 +519,7 @@
                     if(parseInt(datos.num_reg)>0){
                         mensaje('warning', "<h4>Error.</h4> <p>S&oacute;lo se permite realizar una sola devoluci&oacute;n. Realice una anulaci&oacute;n.</p>");
                     }else{
-                        var template = '<h3>Devoluvi&oacute;n de la Venta ' + $('#venta_numero').html().trim() + '</h3>';
+                        var template = '<h3>Devoluci&oacute;n de la Venta ' + $('#venta_numero').html().trim() + '</h3>';
                         template += '<hr class="hr-margin-10">';
                         template += '<h4><label>Productos Devueltos:</label></h4>';
                         $('.producto_detalles_list').each(function () {
