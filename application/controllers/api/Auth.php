23101b08a35b64475de9489556cc5b29943745ca
leosdani <shell_phinx@hotmail.com>
2018-10-12 13:08:42 -0400

    validacion del logo de la empresa

--- application/controllers/api/Auth.php
+++ application/controllers/api/Auth.php
@@ -99,9 +99,14 @@ class Auth extends CI_Controller
 					$version = $this->login_api_model->verificar_version();
 
 					//Emp logo
-					$img_dir = './recursos/img/logo/' . $config['EMPRESA_LOGO'];
-					$image = file_get_contents($img_dir);
-					$emp_logo = base64_encode($image);
+					if (!empty($config['EMPRESA_LOGO'])) {
+						$img_dir = './recursos/img/logo/' . $config['EMPRESA_LOGO'];
+						$image = file_get_contents($img_dir);
+						$emp_logo = base64_encode($image);
+
+					} else {
+						$emp_logo = 'null';
+					}
 
 					// Json Array
 					$json = array(
